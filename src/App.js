ae4acfee4cbfbd240788f608dabc54412cf8d38d
milkshake0 <jiwoo992020@gmail.com>
2021-02-25 20:24:10 +0900

    news viewer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import NewsPage from './pages/NewsPage';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    await axios
-      .get(
-        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=10e11a02ae354fd19874b3bbebffb1e7',
-      )
-      .then((response) => {
-        setData(response.data);
-      });
-  };
-  return (
-    <div className="App">
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly />
-      )}
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 }
 
 export default App;
